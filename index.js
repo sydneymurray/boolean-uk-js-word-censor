7472c0ad560f10f9a78e75b23b714d53620d5b93
sydneymurray <sydney_i_murray@yahoo.co.uk>
2021-04-30 14:19:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const text =
+  "Lorem, ipsum dolor sit amet, consectetur adipisicing elit, \
+  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
+  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit \
+  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+  Excepteur sint occaecat cupidatat non proident, \
+  sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+
+  function sydsReplace(replacer, replacee){
+    alert(text.replace(replacee, replacer));
+  }
+
+  let myReplacee = prompt(`What shall I replace`)  
+  let myReplacer = prompt(`What shall I replace ${myReplacee} with?`)
+    
+  sydsReplace(myReplacer, myReplacee)
+  
+  
