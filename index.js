45f05d94c18653f943d0c8ff6e760cf4d56aaddc
sydneymurray <sydney_i_murray@yahoo.co.uk>
2021-05-01 19:50:09 +0100

    COMPLETED WITH STRETCH 1 & 2

--- index.js
+++ index.js
@@ -8,13 +8,37 @@ const text =
   sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 
-  function sydsReplace(replacer, replacee){
-    alert(text.replace(replacee, replacer));
-  }
+// Sorry guyz, I'm not using your text because I could not tell if my solution was working.
+// Maybe I'm a bit dislexic?
+// So I did a similar solution, but I don't think I did it thee way you wanted
+// Did you want me to turn the string into an array? and complete it that way?
 
-  let myReplacee = prompt(`What shall I replace`)  
-  let myReplacer = prompt(`What shall I replace ${myReplacee} with?`)
-    
-  sydsReplace(myReplacer, myReplacee)
-  
-  
+let uncensoredText =
+  "one two three four five six seven eight nine ten\n \
+   one two three four five six seven eight nine ten\n \
+   one two three four five six seven eight nine ten\n \
+   one two three four five six seven eight nine ten\n \
+   one two three four five six seven eight nine ten\n "
+
+function sydsTextCensorer(wordToCensor, replaceWordWith, textToBeCensored){
+  let censoredText = ""
+  let censoredWordsTally = 0
+  keepCensoringText = 1
+  while (keepCensoringText){
+    censoredText = textToBeCensored.replace(wordToCensor,replaceWordWith)
+    //If no text was censord then exit loop
+    if (censoredText !== textToBeCensored){ 
+      censoredWordsTally++
+      textToBeCensored = censoredText}
+    else 
+      keepCensoringText = 0
+  }
+  return [censoredWordsTally,censoredText]
+}
+
+let textToCensor = prompt(`What word shall I censor?`)  
+let replacementText = prompt(`What shall I replace ${textToCensor} with?`)
+
+let result = sydsTextCensorer(textToCensor, replacementText, uncensoredText)
+alert(`I replaced the word "${textToCensor}" ${result[0]} times`)
+alert('Here is the censored text.\n\n  '+ result[1])
